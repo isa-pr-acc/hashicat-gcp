52b37a2aefb2b4d51ad79ce21ef3f0bfd6dba79d
IsabelPerez <isabel.a.perez@accenture.com>
2023-03-24 17:24:35 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/IsaPR/network/google"
+  version = "3.4.0"
+  # insert required variables here
+   project_id   = var.project
+   network_name = "gaurav-network"
+   subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
